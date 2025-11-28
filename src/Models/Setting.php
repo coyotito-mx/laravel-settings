2de7519c823a65d5babb4be47ca64dad3a8aa740
asciito <ayax.cordova@aydev.mx>
2025-11-28 06:01:58 -0600

    Fix code style issues
    Signed-off-by: asciito <ayax.cordova@aydev.mx>

--- src/Models/Setting.php
+++ src/Models/Setting.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Coyotito\LaravelSettings\Models;
 
-use Coyotito\LaravelSettings\Models\Exceptions\LockedSettingException;
 use Illuminate\Database\Eloquent\Model;
 
 /**
