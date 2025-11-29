ab7e95bf2bd23d6cbb69c225249f32855673e177
asciito <ayax.cordova@aydev.mx>
2025-11-29 14:27:24 -0600

    Fix code style issues

--- rector.php
+++ rector.php
@@ -1,22 +1,23 @@
 <?php
 
 use Rector\Config\RectorConfig;
-use Rector\ValueObject\PhpVersion;
 use Rector\TypeDeclaration\Rector\Property\TypedPropertyFromStrictConstructorRector;
+use Rector\ValueObject\PhpVersion;
 
 return RectorConfig::configure()
     ->withPaths([
-        __DIR__ . '/src',
-        __DIR__ . '/config',
-        __DIR__ . '/tests',
-        __DIR__ . '/database',
+        __DIR__.'/src',
+        __DIR__.'/config',
+        __DIR__.'/tests',
+        __DIR__.'/database',
     ])
     // register single rule
     ->withRules([
-        TypedPropertyFromStrictConstructorRector::class
+        TypedPropertyFromStrictConstructorRector::class,
     ])
     // here we can define, what prepared sets of rules will be applied
     ->withPreparedSets(
+        deadCode: true,
         codeQuality: true
     )
     ->withPhpVersion(PhpVersion::PHP_84);
