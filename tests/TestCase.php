05b026d7f714ab73961e169106be6a472473fe22
asciito <ayax.cordova@aydev.mx>
2025-12-04 00:42:02 -0600

    Remove workbench

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,13 +3,11 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\Concerns\InteractsWithViews;
-use Orchestra\Testbench\Concerns\WithWorkbench;
 use Orchestra\Testbench\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
     use InteractsWithViews;
-    use WithWorkbench;
 
     /**
      * Automatically enables package discoveries.
