78d70d22383396e6191b7b6676741ef32c7c3f55
asciito <ayax.cordova@aydev.mx>
2025-12-13 18:14:20 -0600

    Add test for `Settings` class
    This will use the `DynamicSettings` class which only purpose is to generate dynamic properties in a `SettingsClass` for testing, but the hydration and use of the `Settings` base class is the same.

--- tests/Feature/SettingsTest.php
+++ tests/Feature/SettingsTest.php
@@ -5,15 +5,58 @@ use Coyotito\LaravelSettings\Facades\SettingsManager;
 
 beforeEach(function () {
     rmdir_recursive(app_path('Settings'));
+
+    SettingsManager::clearRegisteredSettingsClasses();
+});
+
+it('can get settings', function () {
+   $settings = Settings::fake([
+       'foo' => 'bar',
+       'bar' => 'baz',
+   ]);
+
+   expect($settings)
+       ->foo->toBe('bar')
+       ->bar->toBe('baz')
+       ->get('foo')->toBe('bar')
+       ->get('bar')->toBe('baz');
+});
+
+it('can update settings', function () {
+    $settings = Settings::fake([
+        'foo' => 'bar',
+        'bar' => 'baz',
+    ]);
+
+    (
+        fn () => expect($this->initialSettings)
+            ->toBe(['foo' => 'bar', 'bar' => 'baz'])
+            ->and($this)
+            ->get('foo')->toBe('bar')
+            ->get('bar')->toBe('baz')
+    )->call($settings);
+
+    $settings->foo = 'Foo';
+
+    expect($settings)
+        ->get('foo')->toBe('Foo')
+        ->get('bar')->toBe('baz')
+        ->save();
+
+    (
+        fn () => expect($this->initialSettings)
+            ->toBe(['foo' => 'Foo', 'bar' => 'baz'])
+            ->and($this)
+            ->get('foo')->toBe('Foo')
+            ->get('bar')->toBe('baz')
+    )->call($settings);
 });
 
 it('can mass update settings', function () {
-    Settings::fake([
+    $settings = Settings::fake([
         'foo' => 'Foo',
         'bar' => 'Bar',
     ]);
-
-    $settings = SettingsManager::resolveSettings(\Coyotito\LaravelSettings\Settings::DEFAULT_GROUP);
 
     expect($settings)
         ->update(['foo' => 'Bar', 'bar' => 'Foo'])
@@ -26,12 +69,10 @@ it('can mass update settings', function () {
 });
 
 it('can mass update only defined settings', function () {
-    Settings::fake([
+    $settings = Settings::fake([
         'foo' => 'Foo',
         'bar' => 'Bar',
     ]);
-
-    $settings = SettingsManager::resolveSettings(\Coyotito\LaravelSettings\Settings::DEFAULT_GROUP);
 
     expect($settings)
         ->update(['foo' => 'Bar', 'bar' => 'Foo'])
