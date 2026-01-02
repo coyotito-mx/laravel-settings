d4076fd7217adf630b4cc9df4442ca851252c0f8
asciito <ayax.cordova@aydev.mx>
2026-01-02 15:00:20 -0600

    Implement `SettingsManifest` class with loading and generating capabilities

--- src/SettingsManifest.php
+++ src/SettingsManifest.php
@@ -5,22 +5,65 @@ declare(strict_types=1);
 
 namespace Coyotito\LaravelSettings;
 
-use League\Flysystem\Filesystem;
+use Coyotito\LaravelSettings\Exceptions\SettingsManifestCannotLoadException;
+use Exception;
+use Illuminate\Filesystem\Filesystem;
 
 class SettingsManifest
 {
-    public function __construct(protected Filesystem $files)
+    /**
+     * Constructor.
+     *
+     * @param Filesystem $files The filesystem instance.
+     * @param string $manifestPath The path to the manifest file.
+     */
+    public function __construct(protected Filesystem $files, protected string $manifestPath)
     {
         //
     }
 
     public function present(): bool
     {
-        //
+        return $this->files->exists($this->getManifestPath());
     }
 
+    /**
+     * Load the settings manifest.
+     *
+     * @return array
+     * @throws Exception if the manifest file is not found
+     */
     public function load(): array
     {
+        if (! $this->present()) {
+            throw new SettingsManifestCannotLoadException("Settings manifest not found at [{$this->getManifestPath()}].");
+        }
 
+        return $this->files->getRequire($this->manifestPath);
+    }
+
+    /**
+     * Generate the settings manifest.
+     *
+     * @template Settings of class-string<Settings>
+     *
+     * @param array<string, Settings> $settings
+     * @return void
+     */
+    public function generate(array $settings): void
+    {
+        $content = '<?php return ' . var_export($settings, true) . ';' . PHP_EOL;
+
+        $this->files->put($this->getManifestPath(), $content);
+    }
+
+    /**
+     * Get the manifest path.
+     *
+     * @return string
+     */
+    public function getManifestPath(): string
+    {
+        return $this->manifestPath;
     }
 }
